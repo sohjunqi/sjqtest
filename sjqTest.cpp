d925951af19bfa132be4e5fb6dd509d74a552d01
sohjunqi <sohjunqi@outlook.com>
2016-09-04 09:59:06 +0800

    Initialization

---
+++ sjqTest.cpp
@@ -0,0 +1,22 @@
+// sjqTest.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+void main()
+{
+	int option = 0;
+
+	do {
+		
+		printf("Please enter a number: ");
+		scanf("%d", &option);
+
+		printf("Your number was %d\n", option);
+
+
+	} while (option != 0);
+	
+}
+
